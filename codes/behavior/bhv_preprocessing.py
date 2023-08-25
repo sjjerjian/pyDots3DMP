7738e74848bc3bfdd3f4cfef9e1855a7d9c16be1
Steven Jerjian <sjerjian@jhu.edu>
2023-08-25 11:12:16 -0400

    move trial_list generation to preprocessing module

--- codes/behavior/bhv_preprocessing.py
+++ codes/behavior/bhv_preprocessing.py
@@ -122,5 +122,63 @@ def data_cleanup(filename: str, drop_cols=None, save_file: bool = False) -> pd.D
 
     if save_file:
         bhv_df_clean.to_csv(PurePath(folder, clean_filename))
-    
-    return bhv_df_clean
+    else:
+        return bhv_df_clean
+
+
+def dots3DMP_create_trial_list(hdgs: list, mods: list, cohs: list, deltas: list,
+                               nreps: int = 1, shuff: bool = True) -> tuple[pd.DataFrame, int]:
+
+    # if shuff:
+    #     np.random.seed(42)  # for reproducibility
+
+    num_hdg_groups = any([1 in mods]) + any([2 in mods]) * len(cohs) + \
+        any([3 in mods]) * len(cohs) * len(deltas)
+    hdg = np.tile(hdgs, num_hdg_groups)
+
+    coh = np.empty_like(hdg)
+    modality = np.empty_like(hdg)
+    delta = np.empty_like(hdg)
+
+    if 1 in mods:
+        coh[:len(hdgs)] = cohs[0]
+        delta[:len(hdgs)] = 0
+        modality[:len(hdgs)] = 1
+        last = len(hdgs)
+    else:
+        last = 0
+
+    # visual has to loop over cohs
+    if 2 in mods:
+        for c in range(len(cohs)):
+            these = slice(last + c*len(hdgs), last + c*len(hdgs) + len(hdgs))
+            coh[these] = cohs[c]
+            delta[these] = 0
+            modality[these] = 2
+        last = these.stop
+
+    # combined has to loop over cohs and deltas
+    if 3 in mods:
+        for c in range(len(cohs)):
+            for d in range(len(deltas)):
+                here = last + c*len(hdgs)*len(deltas) + d*len(hdgs)
+                these = slice(here, here + len(hdgs))
+                coh[these] = cohs[c]
+                delta[these] = deltas[d]
+                modality[these] = 3
+
+    # Now replicate times nreps and shuffle (or not):
+    condlist = np.column_stack((modality, coh, hdg, delta))
+    trial_table = np.tile(condlist, (nreps, 1))
+    ntrials = len(trial_table)
+
+    if shuff:
+        trial_table = trial_table[np.random.permutation(ntrials)]
+
+    trial_table = pd.DataFrame(trial_table, columns=['modality', 'coherence', 'heading', 'delta'])
+
+    return trial_table, ntrials
+
+
+if __name__ == "__main__":
+    data = data_cleanup("lucio", (20220512, 20230605))
