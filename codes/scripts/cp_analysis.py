c3ec242146eb203b30332582dd6ab1182dbad99e
Steven Jerjian <sjerjian@jhu.edu>
2023-09-05 20:34:28 -0400

    update scripts

---
+++ codes/scripts/cp_analysis.py
@@ -0,0 +1,71 @@
+# %% ========================
+# imports 
+
+import numpy as np
+import pandas as pd
+import pickle
+from pathlib import PurePath
+
+from behavior.preprocessing import dots3DMP_create_trial_list, add_trial_outcomes
+from neural.rate_utils import build_pseudopop, concat_aligned_rates
+from neural.tuning_utils import *
+
+# %% ========================
+# load data 
+
+data_folder = '/Users/stevenjerjian/Desktop/FetschLab/Analysis/data'
+filename = 'lucio_20220512-20230602_neuralData.pkl'
+filename = PurePath(data_folder, 'lucio_neuro_datasets', filename)
+
+with open(filename, 'rb') as file:
+    data = pd.read_pickle(file)
+
+pars = ['Tuning', 'Task']
+data = data.loc[data[pars].notna().all(axis=1) & data['is_good']]
+
+# %% ========================
+# define conditions of interest 
+
+# these are the same for tuning and task
+cond_labels = ['modality', 'coherenceInd', 'delta', 'heading', 'choice', 'PDW', 'oneTargConf']
+mods = [1, 2, 3]
+cohs = [1, 2]
+deltas = [0]
+hdgs_task = [-12, -6, -3, -1.5, 0, 1.5, 3, 6, 12]
+
+outcomes = {'choice': [1, 2], 'PDW': [0, 1], 'oneTargConf': [0]}
+tr_tab_task = dots3DMP_create_trial_list(hdgs_task, mods, cohs, deltas, 1, shuff=False).pipe(
+    add_trial_outcomes, outcomes)
+tr_tab_task.columns = cond_labels  
+
+
+# %% ========================
+# for choice probability calculations, we need single trial firing rates
+
+t_params = {'align_ev': ['stimOn', 'saccOnset'],
+            'trange': np.array([[-1.5, 1.3], [-0.5, 1.5]]), 
+            'other_ev': [['fpOn', 'fixation', 'targsOn', 'saccOnset'], ['postTargHold']],
+            'binsize': 0.05}
+
+sm_params = {'type': 'gaussian',
+             'binsize': t_params['binsize'],
+             'width': 0.4, 'sigma': 0.05}
+
+rates_task, units_task, conds_task, _, _ = \
+    zip(*data['Task'].apply(lambda x: x.get_firing_rates(align_ev=t_params['align_ev'],
+                                                         trange=t_params['trange'],
+                                                         binsize=t_params['binsize'],
+                                                         sm_params=sm_params,
+                                                         condlabels=cond_labels)
+                                                         ))
+
+
+
+sm_params = {'type': 'gaussian',
+             'binsize': t_params['binsize'],
+             'width': 0.4, 'sigma': 0.05}
+
+task_pseudopop = build_pseudopop(popn_dfs=data['Task'], tr_tab=tr_tab_task,
+    t_params=t_params, smooth_params=sm_params, return_averaged=True)
+
+# %%
--- codes/scripts/script_pseudopop_tuning.py
+++ codes/scripts/script_pseudopop_tuning.py
@@ -1,33 +1,38 @@
-# %% imports
+# %% ========================
+# imports 
 
 import numpy as np
-
-from behavior.preprocessing import dots3DMP_create_trial_list
-from neural.rate_utils import build_pseudopop
+import pandas as pd
 import pickle
 from pathlib import PurePath
 
-# %% ===== load data =====
+from behavior.preprocessing import dots3DMP_create_trial_list, add_trial_outcomes
+from neural.rate_utils import build_pseudopop, concat_aligned_rates
+from neural.tuning_utils import *
+
+# %% ========================
+# load data 
 
 data_folder = '/Users/stevenjerjian/Desktop/FetschLab/Analysis/data'
 filename = 'lucio_20220512-20230602_neuralData.pkl'
 filename = PurePath(data_folder, 'lucio_neuro_datasets', filename)
 
 with open(filename, 'rb') as file:
-    this_df = pickle.load(file)
+    data = pd.read_pickle(file)
 
 pars = ['Tuning', 'Task']
-data = this_df.loc[this_df[pars].notna().all(axis=1) & this_df['is_good']]
+data = data.loc[data[pars].notna().all(axis=1) & data['is_good']]
 
-# %% ===== define conditions of interest =====
+# %% ========================
+# define conditions of interest 
 
 # these are the same for tuning and task
-cond_labels = ['modality', 'coherenceInd', 'heading', 'delta']
+cond_labels = ['modality', 'coherenceInd', 'delta', 'heading']
 mods = [1, 2, 3]
 cohs = [1, 2]
 deltas = [0]
 
-# ...and headings are different
+# headings are different...
 hdgs_tuning = [-60, -45, -25, -22.5, -12, 0, 12, 22.5, 25, 45, 60]
 hdgs_task = [-12, -6, -3, -1.5, 0, 1.5, 3, 6, 12]
 
@@ -39,21 +44,73 @@ tr_tab_tuning.columns = cond_labels  # should use .rename here to make sure orde
 tr_tab_task, _ = dots3DMP_create_trial_list(hdgs_task, mods, cohs, deltas, 1, shuff=False)
 tr_tab_task.columns = cond_labels  
 
-# %% ===== Create tuning pseudopopulation =====
+# %% ========================
+# Create tuning pseudopopulation 
 
 t_params = {'align_ev': [['stimOn', 'stimOff'], 'fpOn'], 
             'trange': np.array([[0.5, -0.5], [0, 0.5]]), 
             'binsize': 0}
 
 tuning_pseudopop = build_pseudopop(popn_dfs=data['Tuning'], tr_tab=tr_tab_tuning, 
-                                   t_params=t_params, return_averaged=True
+                                   t_params=t_params, return_averaged=True,
 )
 
-# %% ===== Create task pseudopopulation (time-resolved) =====
+# %% ========================
+# Calculating tuning significance (across headings and across baseline/stimulus)
+
+rates_tuning, units_tuning, conds_tuning, _, _ = \
+    zip(*data['Tuning'].apply(lambda x: x.get_firing_rates(align_ev=t_params['align_ev'],
+                                                           trange=t_params['trange'],
+                                                           binsize=t_params['binsize'],
+                                                           condlabels=cond_labels)
+                                                           ))
+
+rates_tuning_cat, _ = concat_aligned_rates(rates_tuning)
+
+tuning_res_within = {'stats': [], 'pvals': [], 'unique_conds': []}
+tuning_res_across = {'stats': [], 'pvals': [], 'unique_conds': []}
+for f_in, cond in zip(rates_tuning_cat, conds_tuning):
+
+    stats, pvals, cg = tuning_across(f_rates=f_in, condlist=cond, cond_groups=tr_tab_tuning,
+                                     cond_cols=['modality', 'coherenceInd'], tuning_col='heading',
+                                     bsln_t=1, abs_diff=True, parametric=True)
+    
+    tuning_res_across['stats'].append(stats)
+    tuning_res_across['pvals'].append(pvals)
+    tuning_res_across['unique_conds'].append(cg)
+    
+    stats, pvals, cg = tuning_within(f_rates=f_in, condlist=cond, cond_groups=tr_tab_tuning,
+                                     cond_cols=['modality', 'coherenceInd'], tuning_col='heading',
+                                     parametric=True)
+    
+    tuning_res_within['stats'].append(stats)
+    tuning_res_within['pvals'].append(pvals)
+    tuning_res_within['unique_conds'].append(cg)
+
+
+tuning_conds = tuning_res_across['unique_conds'][0]
+pvals_across = np.vstack(tuning_res_across['pvals'])
+pvals_within = np.vstack(tuning_res_within['pvals'])
+
+area = np.array(tuning_pseudopop.area)
+print(f"MSTd n={np.sum(area=='MSTd')}, PIVC n={np.sum(area=='PIVC')}")
+MSTsu = np.sum((area=='MSTd') & (tuning_pseudopop.clus_group==2))
+PIVCsu = np.sum((area=='PIVC') & (tuning_pseudopop.clus_group==2))
+
+print(f"MSTd n={MSTsu}, PIVC n={PIVCsu}")
+
+
+np.array([(p=='MSTd') and not low and u==2 for p, low, u in zip(pseudo_pop.area, lf, pseudo_pop.clus_group)]).sum()
+np.array([(p=='PIVC') and not low and u==2 for p, low, u in zip(pseudo_pop.area, lf, pseudo_pop.clus_group)]).sum()
+
+
+
+# %% ========================
+# # ===== Create task pseudopopulation (time-resolved) =====
 
 t_params = {'align_ev': ['stimOn', 'saccOnset'],
             'trange': np.array([[-1.5, 1.3], [-0.5, 1.5]]), 
-            'other_ev': [['fpOn', 'fixation', 'targsOn', 'saccOnset'], ['WagerHold']],
+            'other_ev': [['fpOn', 'fixation', 'targsOn', 'saccOnset'], ['postTargHold']],
             'binsize': 0.05}
 
 sm_params = {'type': 'gaussian',
@@ -61,44 +118,21 @@ sm_params = {'type': 'gaussian',
              'width': 0.4, 'sigma': 0.05}
 
 task_pseudopop = build_pseudopop(popn_dfs=data['Task'], tr_tab=tr_tab_task,
-    t_params=t_params, smooth_params=sm_params,
-    return_averaged=True
-    )
-
-# %% ===== Create PseudoPopulation =====
-
-use_single_trials = False
-
-if not use_single_trials:
-    # extract condition averaged FRs
-
-    rates_cat, len_intervals = rates.concat_aligned_rates(f_rates, tvecs)
-
-    cond_frs, cond_groups = [], []
-    for f_in, cond in zip(rates_cat, conds):
-
-        # avg firing rate over time across units, each cond, per session
-        f_out, _, cg = rates.condition_averages(f_in, cond, cond_groups=tr_tab)
-        cond_frs.append(f_out)
-        cond_groups.append(cg)
-
-    # resplit by len_intervals, for pseudopop creation
-    f_rates = list(map(lambda f, x: np.split(f, x, axis=2)[:-1], cond_frs, len_intervals))
-
-    conds = cond_groups
-
-    # TODO pass in other metadata - e.g. alignments and fr calc parameters (sm params, binsize)
+    t_params=t_params, smooth_params=sm_params, return_averaged=True)
 
 
-# now create the pseudopopulation
-pseudo_pop = build_pseudopop(
-    fr_list=f_rates,
-    conds_dfs=conds,
-    unitlabels=units,
-    areas=np.array(data[expt].apply(lambda x: x.area)),
-    subject="lucio",
-    tvecs=tvecs,
-)
+# %% ========================
+
+# TODO ANALYSES
+
+# tuning of each unit during task
+# significance of heading tuning in task, variation from baseline, confidence and choice separation
+# 'choice probability', 'wager probability'
+# 
+# regression onto stimulus/decision variables (need acc/vel from nexonar) also use fixation, fixation point, targets, stimulus conditions, choice and wager outcomes (1/-1)
+# construct matrix of regressors
+# this is on trial-averages, aligned to stimOn or saccOnset
+
 
 # import matplotlib.pyplot as plt
 # for al in range(len(pseudo_pop.firing_rates)):
@@ -106,6 +140,11 @@ pseudo_pop = build_pseudopop(
 #     plt.plot(pseudo_pop.timestamps[al], pseudo_pop.firing_rates[al][0, :, :].T)
 #
 
+
+
+
+# %% ========================
+# save pseudo-pop to file, if desired
 
 print(f"Pseudopopulation built for {filename}")
 
